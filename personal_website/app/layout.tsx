79454da9cffea93e9749f21f0af29dba5836390d
zubsyeda <125157533+zubsyeda@users.noreply.github.com>
2024-07-27 03:13:48 +0000

    Initial done

---
+++ personal_website/app/layout.tsx
@@ -0,0 +1,19 @@
+import { Inter } from 'next/font/google'
+ 
+// If loading a variable font, you don't need to specify the font weight
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+})
+ 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" className={inter.className}>
+      <body>{children}</body>
+    </html>
+  )
+}
